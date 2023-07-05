0f898a718e4909ee5e0e3843e2af983069c9f426
Ef <efratmatalon2020@gmail.com>
2023-07-05 16:19:44 +0300

    stor client side

--- src/components/payment.tsx
+++ src/components/payment.tsx
@@ -9,18 +9,18 @@ import { getById } from "../store/function/categoryThunk"
 const Payment = () => {
     const selectorSumOfAll = useSelector((x: any) => x.BugReducer.sumOfAll)
     const s = () => {
-        swal("הקניה בוצעה בהצלחה","","success")
+        swal("הקניה בוצעה בהצלחה", "", "success")
     }
     return <div style={{ textAlign: "center" }}>
         <h2>סכום לתשלום: {selectorSumOfAll} ש"ח</h2>
         <h3>הזן פרטי אשראי</h3>
         <br></br><br></br>
         <div style={{ textAlign: "center" }} className="col-xs-3">
-        <input className="form-control" id="ex2" type="text" placeholder="מספר הכרטיס" ></input>
-        <br></br><br></br>
-        <input className="form-control" id="ex2" type="text" placeholder="שלוש ספרות בגב הכרטיס " ></input>
-        <br></br><br></br>
-        <button type="submit" className="myButton" onClick={() => s()}>סיום</button>
+            <input className="form-control" id="ex2" type="text" placeholder="מספר הכרטיס" ></input>
+            <br></br><br></br>
+            <input className="form-control" id="ex2" type="text" placeholder="שלוש ספרות בגב הכרטיס " ></input>
+            <br></br><br></br>
+            <button type="submit" className="myButton" onClick={() => s()}>סיום</button>
         </div>
     </div>
 }
